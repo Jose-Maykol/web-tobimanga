2d0d5d201db0d91bfff6d98a7cb00a3b04e1b78c
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-08-03 22:09:07 -0500

    feat: add api authentication interceptor

---
+++ src/interceptors/api-auth-interceptor.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { cookies } from 'next/headers'
+
+const apiAuth = axios.create({
+	baseURL: 'http://localhost:8000/api',
+	timeout: 5000
+})
+
+apiAuth.interceptors.request.use(
+	(config) => {
+		const cookieStore = cookies()
+		const token = cookieStore.get('token')
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`
+		}
+		return config
+	},
+	(error) => {
+		return Promise.reject(error)
+	}
+)
+
+apiAuth.interceptors.response.use(
+	(response) => {
+		return response
+	},
+	(error) => {
+		if (error.response?.status === 401) {
+			console.warn('Unauthorized')
+		}
+		return Promise.reject(error)
+	}
+)
+
+export default apiAuth
