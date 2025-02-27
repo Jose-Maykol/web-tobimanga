d43ab4bcc6407908f49ed2d1237c85d894707e20
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-02-26 22:47:07 -0500

    feat: enhance start following reading button with status selection and toast notifications

--- src/app/(main)/manga/[slug]/_components/start-following-reading-button.tsx
+++ src/app/(main)/manga/[slug]/_components/start-following-reading-button.tsx
@@ -32,6 +32,8 @@ const allowedStatuses: ReadingStatus[] = [
 export default function StartFollowingReadingButton() {
 	const { isAuthenticated } = useAuthStore()
 	const [isFollowing, setIsFollowing] = useState(false)
+	const [isOpen, setIsOpen] = useState(false)
+	const [selectedStatus, setSelectedStatus] = useState<ReadingStatus>(ReadingStatus.READING)
 	const router = useRouter()
 
 	const handleFollow = () => {
@@ -43,21 +45,36 @@ export default function StartFollowingReadingButton() {
 		toast.success('¡Ahora sigues este manga!')
 	}
 
+	const handleOptionSelect = (status: ReadingStatus) => {
+		setSelectedStatus(status)
+		toast.success(`¡Estado actualizado! Ahora estás ${status.toLowerCase()}`)
+		setIsOpen(false)
+	}
+
 	if (isFollowing) {
 		return (
 			<div>
-				<Select>
+				<Select
+					onValueChange={(value) => setSelectedStatus(value as ReadingStatus)}
+					open={isOpen}
+					onOpenChange={setIsOpen}
+				>
 					<SelectTrigger className='bg-neutral-200 text-black focus:ring-0'>
-						<SelectValue placeholder={'Siguendo'} />
+						<BookOpen className='mr-2 h-4 w-4' />{' '}
+						<p>{readingStatusOptions.find((option) => option.value === selectedStatus)?.label}</p>
 					</SelectTrigger>
 					<SelectContent align='end' className='w-[200px]'>
 						<SelectGroup>
 							{readingStatusOptions
 								.filter((option) => allowedStatuses.includes(option.value))
 								.map((option) => (
-									<SelectItem key={option.value} value={option.value}>
-										<SelectLabel className='text-center'>{option.label}</SelectLabel>
-									</SelectItem>
+									<div
+										key={option.value}
+										className='cursor-pointer hover:bg-neutral-700 p-2 rounded-sm text-sm text-center'
+										onClick={() => handleOptionSelect(option.value)}
+									>
+										<span className='text-center'>{option.label}</span>
+									</div>
 								))}
 						</SelectGroup>
 					</SelectContent>
